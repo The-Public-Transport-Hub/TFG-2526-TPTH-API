75e1e64d3d276614e0d8a7bb8515135096ea48fc
Eduardosntr <alu0101565590@ull.edu.es>
2026-06-29 17:17:58 +0100

    Fix error in url

--- src/features/stops/infrastructure/providers/metroTenerife/config.ts
+++ src/features/stops/infrastructure/providers/metroTenerife/config.ts
@@ -1,3 +1,3 @@
 export const provider = "Metro Tenerife"
 export const tramStopsOpenDataUrl = "https://datos.tenerife.es/ckan/dataset/19914413-77e1-441c-83a7-8f0f45c0767a/resource/2795c9fd-bb42-4270-ae71-23ed11c88610/download/paradas-de-tranvia.geojson"
-export const tramStopDetailsUrl = "https://metro-tenerife-proxy.alu0101565590.workers.dev/""
+export const tramStopDetailsUrl = "https://metro-tenerife-proxy.alu0101565590.workers.dev/";
